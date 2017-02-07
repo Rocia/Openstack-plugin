0542aab6e6ffb0b3ca6d8690dd75ebc1180c0083
Rocia <rocia.fernandes@gmail.com>
2017-02-08 01:13:53 +0530

    let a. txt be the user text file

--- pluginCode.py
+++ pluginCode.py
@@ -2,4 +2,27 @@ import math
 file = open ('a.txt', "r")
 data = file.read()
 asci = [ord(c) for c in data]
+file.close()
 print(asci)
+
+x = len(asci)
+print (x)
+print(type(x))
+n= 0
+status = False
+print (math.sqrt(x))
+if isinstance(math.sqrt(x), int):
+    print(Hi)
+    status = True
+
+elif isinstance(math.sqrt(x), float):
+    status = False
+    while status == False:
+        n+1
+        x+1
+        if type(math.sqrt(x)) == int:
+            status = True
+
+print(n)
+
+
