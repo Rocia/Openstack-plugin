fc9f92beecc6830885a33d7d96a23f2e45e1aad3
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-03-29 03:04:00 +0530

    minor changes

--- shamirs.py
+++ shamirs.py
@@ -1,5 +1,12 @@
 import math
 import random
+import itertools
+
+def isPrime(n):
+    for m in range(2, int(n**0.5)+1):
+        if not n%m:
+            return False
+    return True
 
 file = open ('a.txt', "r")
 content = file.read()
@@ -21,34 +28,23 @@ for n,i in enumerate(asci):
 print(asci)
 
 data = ''.join(map(str, asci))
-'''
-for i in range(0,len(asci)):
-    data[i] += asci[i]
-for asci[i] in range(0,len):
-    data=str[i] + str[i+1]
-print(data)
 
-data = 0
-for i in asci:
-    ''.join(data)'''
 print(data)
 
 #data = 1234
-
 #data = int(asci)
-'''for c in asci:
-    if c in range(0,99):
-        c= 0+'c'
-print(asci)
-'''
 # convert to 3 dig format
 # if dig in first position is 0 or ~ is the first char of document append ascii equivalent of ~
 # number of nodes active are detected
 n = random.randint(1, 10)
 
 # rand function for random prime numbers
-R1= random.randint(1, 1000)
-R2= random.randint(1, 1000)
+x = len(data)
+l = '1'+('0'*x)
+length = int (l)
+
+R1= random.randint(length/2,length)
+R2= random.randint(length/2,length)
 nodes = {}
 
 # create polynomial as data + rand1X+ rand2X^2
@@ -57,7 +53,7 @@ if(n <4 ):
     print ("The system is currently incapable of securing your data. Please try again later.")
 else:
     for i in range (0, n):
-        x = random.randint(1, 1000)
+        x = next(i for i in itertools.imap(lambda x: random.randint(1,1000)|1,itertools.count()) if isPrime(i))
         H= int(data) + (R1*x) + (R2*x*x)
         nodes[x] = H
 print (nodes)
